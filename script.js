22cae47b551990624f01209d75916b3b8de9a7bd
Luna Z Y <lunazy@hotmail.com>
2024-02-14 18:22:51 +0200

    ultimos cambios

--- script.js
+++ script.js
@@ -1,61 +1,51 @@
 // Capturo botones y variables
-const botoCity = document.querySelector(".edit__button")
+const botoCity = document.querySelector(".edit__button");
 
-const popUpCity= document.querySelector(".item__form")
+const popUpCity = document.querySelector(".item__form");
 
-botoCity.addEventListener("click",()=>abrirCity())
+botoCity.addEventListener("click", () => openCity());
 
-function abrirCity(){
-    popUpCity.classList.remove("item__form-disable")
-   
+function openCity() {
+  popUpCity.classList.remove("item__form-disable");
 }
 
-const closeButton= document.querySelector(".close-button")
+const closeButton = document.querySelector(".close__button");
 
+closeButton.addEventListener("click", () => closeCity());
 
-closeButton.addEventListener("click",()=>cerrarCity())
-
-function cerrarCity(){
-    popUpCity.classList.add("item__form-disable")
+function closeCity() {
+  popUpCity.classList.add("item__form-disable");
 }
-
-
 
 //funcion eliminar trash
-const deleteButton = document.querySelectorAll(".element__trash")
+const deleteButton = document.querySelectorAll(".element__trash");
 
-deleteButton.forEach((e)=>{
-e.addEventListener("click",(e)=>eliminar(e))
-})
+deleteButton.forEach((e) => {
+  e.addEventListener("click", (e) => eliminar(e));
+});
 
-function eliminar(e){
-e.srcElement.offsetParent.remove()
+function eliminar(e) {
+  e.srcElement.offsetParent.remove();
 }
 
+//funcion boton crear
+const submitButton = document.querySelector(".create__button");
 
-//funcion boton crear 
-const submitButton = document.querySelector(".create-button")
-
-submitButton.addEventListener("click",()=>cerrarCity())
-function crearCity(){
-    popUpCity.classList.add("create-button")
+submitButton.addEventListener("click", () => closeCity());
+function crearCity() {
+  popUpCity.classList.add("create__button");
 }
 
+const inputName = document.querySelector("#name");
+const inputProfesion = document.querySelector("#profesion");
 
+const profileName = document.querySelector("#profile-name");
+const profileProfesion = document.querySelector("#profile-profesion");
 
-
-const inputName = document.querySelector("#name")
-const inputProfesion = document.querySelector("#profesion")
-
-const profileName = document.querySelector("#profile-name")
-const profileProfesion = document.querySelector("#profile-profesion")
-
-const profileForm = document.querySelector("#profile-form")
+const profileForm = document.querySelector("#profile-form");
 
 profileForm.addEventListener("submit", function (evt) {
-    evt.preventDefault()
-    profileName.textContent = inputName.value 
-    profileProfesion.textContent = inputProfesion.value 
-})
-
-
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileProfesion.textContent = inputProfesion.value;
+});
